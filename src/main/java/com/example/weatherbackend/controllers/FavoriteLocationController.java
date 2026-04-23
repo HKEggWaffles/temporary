c83be5ae69307e4be186c7ed43de86198c272397
HKEggWaffles <jinjuthegingu@gmail.com>
2026-04-23 22:30:12 +0100

    WIP: before debugging branch

--- src/main/java/com/example/weatherbackend/controllers/FavoriteLocationController.java
+++ src/main/java/com/example/weatherbackend/controllers/FavoriteLocationController.java
@@ -1,36 +1,38 @@
 package com.example.weatherbackend.controllers;
 
+import com.example.weatherbackend.dto.locationReqest;
 import com.example.weatherbackend.models.FavoriteLocation;
-import com.example.weatherbackend.repos.FavoriteLocationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.weatherbackend.services.logic;
+
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/favorites")
-@CrossOrigin(origins = {
-    "http://127.0.0.1:5500",
-    "http://localhost:5500"
-})
+@CrossOrigin(origins = "http://localhost:5500")
 public class FavoriteLocationController {
 
-    @Autowired
-    private FavoriteLocationRepository favoriteLocationRepository;
+    private final FavoriteLocationService favoriteLocationService;
+
+    public FavoriteLocationController(FavoriteLocationService favoriteLocationService) {
+        this.favoriteLocationService = favoriteLocationService;
+    }
 
     @GetMapping
     public Iterable<FavoriteLocation> getAllFavorites() {
-        return favoriteLocationRepository.findAll();
+        return favoriteLocationService.getAllFavorites();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public FavoriteLocation addFavorite(@RequestBody FavoriteLocation favoriteLocation) {
-        return favoriteLocationRepository.save(favoriteLocation);
+    public FavoriteLocation addFavorite(@Valid @RequestBody FavoriteLocationRequest request) {
+        return favoriteLocationService.addFavorite(request);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteFavorite(@PathVariable Long id) {
-        favoriteLocationRepository.deleteById(id);
+        favoriteLocationService.deleteFavorite(id);
     }
 }
